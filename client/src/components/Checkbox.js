514f7aff90ea8b30c3755576abf85fc01b912f8b
Ben Hancock <hancocb@purdue.edu>
2019-10-18 15:01:54 -0400

    revise format of checkbox component

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-function Checkbox({ field, type, label }) {
-  return (
-    <div className="form-check">
-      <input
-        className="form-check-input"
-        {...field}
-        type={type}
-        id={field.name}
-      />
-      <label className="form-check-label" htmlFor={field.name}>
-        {label}
-      </label>
-    </div>
-  );
-}
+const Checkbox = ({ field, type, label }) => (
+  <div className="form-check">
+    <input
+      className="form-check-input"
+      {...field}
+      type={type}
+      id={field.name}
+    />
+    <label className="form-check-label" htmlFor={field.name}>
+      {label}
+    </label>
+  </div>
+);
 
 export default Checkbox;
