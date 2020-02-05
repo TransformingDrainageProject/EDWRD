2c9be8c1ce05cca505bf2789091e88d66320fc28
Ben Hancock <hancocb@purdue.edu>
2020-02-05 10:02:40 -0500

    rearrange soil profile settings and reservoir settings in advanced settings modal

--- client/src/components/Form/AdvancedSettingsForm/AdvancedSettingsForm.js
+++ client/src/components/Form/AdvancedSettingsForm/AdvancedSettingsForm.js
@@ -61,128 +61,139 @@ const AdvancedSettings = props => {
         <ModalHeader toggle={toggle}>Advanced Settings</ModalHeader>
         <ModalBody>
           <Container>
-            <Row className="mb-3" style={{ border: '1px solid #c8ced5' }}>
-              <Col>
-                <Row>
+            <Row className="mb-3">
+              <Col md={8}>
+                <Row className="mr-1" style={{ border: '1px solid #c8ced5' }}>
                   <Col>
-                    <h4>
-                      <u>Reservoir Settings</u>
-                    </h4>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md="4">
-                    <AdvancedLabel
-                      name="rseep"
-                      text="Reservoir seepage rate"
-                      unit="inchDay"
-                      unitType={unitType}
-                    >
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="rseep"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="rseep" />
+                    <Row>
+                      <Col>
+                        <h4>
+                          <u>Soil Profile Settings</u>
+                        </h4>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={6}>
+                        <AdvancedLabel
+                          name="zrfc"
+                          text="Soil profile field capacity"
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="zrfc"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="zrfc" />
+                      </Col>
+                      <Col md={6}>
+                        <AdvancedLabel
+                          name="zrwp"
+                          text="Soil profile wilting point"
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="zrwp"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="zrwp" />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={6}>
+                        <AdvancedLabel
+                          name="ze"
+                          text="Depth of soil evaporation layer"
+                          unit="feet"
+                          unitType={unitType}
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="ze"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="ze" />
+                      </Col>
+                      <Col md={6}>
+                        <AdvancedLabel
+                          name="rew"
+                          text="Readily evaporable water"
+                          unit="inches"
+                          unitType={unitType}
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="rew"
+                            step="0.1"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="rew" />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={6}>
+                        <AdvancedLabel
+                          name="zefc"
+                          text="Surface field capacity"
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="zefc"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="zefc" />
+                      </Col>
+                      <Col md={6}>
+                        <AdvancedLabel name="zewp" text="Surface wilting point">
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="zewp"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="zewp" />
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </Col>
-            </Row>
-            <Row className="mb-3" style={{ border: '1px solid #c8ced5' }}>
-              <Col>
-                <Row>
+              <Col md={4}>
+                <Row style={{ border: '1px solid #c8ced5' }}>
                   <Col>
-                    <h4>
-                      <u>Soil Profile Settings</u>
-                    </h4>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <AdvancedLabel
-                      name="zrfc"
-                      text="Soil profile field capacity"
-                    >
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="zrfc"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="zrfc" />
-                  </Col>
-                  <Col>
-                    <AdvancedLabel
-                      name="zrwp"
-                      text="Soil profile wilting point"
-                    >
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="zrwp"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="zrwp" />
-                  </Col>
-                  <Col>
-                    <AdvancedLabel
-                      name="ze"
-                      text="Depth of soil evaporation layer"
-                      unit="feet"
-                      unitType={unitType}
-                    >
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="ze"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="ze" />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <AdvancedLabel name="zefc" text="Surface field capacity">
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="zefc"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="zefc" />
-                  </Col>
-                  <Col>
-                    <AdvancedLabel name="zewp" text="Surface wilting point">
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="zewp"
-                        step="0.01"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="zewp" />
-                  </Col>
-                  <Col>
-                    <AdvancedLabel
-                      name="rew"
-                      text="Readily evaporable water"
-                      unit="inches"
-                      unitType={unitType}
-                    >
-                      <Field
-                        className="form-control"
-                        type="number"
-                        name="rew"
-                        step="0.1"
-                      />
-                    </AdvancedLabel>
-                    <ErrorMessage name="rew" />
+                    <Row>
+                      <Col>
+                        <h4>
+                          <u>Reservoir Settings</u>
+                        </h4>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <AdvancedLabel
+                          name="rseep"
+                          text="Reservoir seepage rate"
+                          unit="inchDay"
+                          unitType={unitType}
+                        >
+                          <Field
+                            className="form-control"
+                            type="number"
+                            name="rseep"
+                            step="0.01"
+                          />
+                        </AdvancedLabel>
+                        <ErrorMessage name="rseep" />
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </Col>
