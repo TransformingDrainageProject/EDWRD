7c1327f3e032680d16d45231a57b1b40ba5fc935
Ben Hancock <hancocb@purdue.edu>
2020-08-20 16:10:54 -0400

    add reactstrap tooltips for variable buttons

--- client/src/components/Charts/NutrientCaptureOverflow/VariableButtons.js
+++ client/src/components/Charts/NutrientCaptureOverflow/VariableButtons.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, ListGroup, ListGroupItem, Row } from 'reactstrap';
+
+import MyTooltip from '../MyTooltip';
 
 import chartVariables from './variables';
 import updateActive from '../utils/updateActive';
@@ -11,144 +13,198 @@ const VariableButtons = ({
   selectedVol,
   setActive,
   updateSelectedChartData,
-}) => (
-  <>
-    <Row className="text-center">
-      <Col md={4}>
-        <strong>Nitrate</strong>
-      </Col>
-      <Col md={4}>
-        <strong>Reactive Phosphorus</strong>
-      </Col>
-      <Col md={4}>
-        <strong>Nutrient Load</strong>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={4}>
-        <ListGroup style={{ lineHeight: '1.0rem' }}>
-          <ListGroupItem
-            active
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[0].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[0].key)}
-          >
-            {chartVariables[0].title}
-          </ListGroupItem>
-          <ListGroupItem
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[1].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[1].key)}
-          >
-            {chartVariables[1].title}
-          </ListGroupItem>
-        </ListGroup>
-      </Col>
-      <Col md={4}>
-        <ListGroup style={{ lineHeight: '1.0rem' }}>
-          <ListGroupItem
-            active
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[2].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[2].key)}
-          >
-            {chartVariables[2].title}
-          </ListGroupItem>
-          <ListGroupItem
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[3].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[3].key)}
-          >
-            {chartVariables[3].title}
-          </ListGroupItem>
-        </ListGroup>
-      </Col>
-      <Col md={4}>
-        <ListGroup style={{ lineHeight: '1.0rem' }}>
-          <ListGroupItem
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[4].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[4].key)}
-          >
-            {chartVariables[4].title}
-          </ListGroupItem>
-          <ListGroupItem
-            tag="button"
-            action
-            onClick={() =>
-              updateActive(
-                chartVariables[5].key,
-                active,
-                setActive,
-                chartData,
-                annualFilter,
-                selectedVol,
-                updateSelectedChartData
-              )
-            }
-            active={active.includes(chartVariables[5].key)}
-          >
-            {chartVariables[5].title}
-          </ListGroupItem>
-        </ListGroup>
-      </Col>
-    </Row>
-  </>
-);
+}) => {
+  const [tooltipOpen, setTooltipOpen] = useState(
+    new Array(chartVariables.length).fill(false)
+  );
+
+  return (
+    <>
+      <Row className="text-center">
+        <Col md={4}>
+          <strong>Nitrate</strong>
+        </Col>
+        <Col md={4}>
+          <strong>Reactive Phosphorus</strong>
+        </Col>
+        <Col md={4}>
+          <strong>Nutrient Load</strong>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4}>
+          <ListGroup style={{ lineHeight: '1.0rem' }}>
+            <ListGroupItem
+              id="variable0"
+              active
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[0].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[0].key)}
+            >
+              {chartVariables[0].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={0}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[0].description}
+            </MyTooltip>
+            <ListGroupItem
+              id="variable1"
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[1].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[1].key)}
+            >
+              {chartVariables[1].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={1}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[1].description}
+            </MyTooltip>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <ListGroup style={{ lineHeight: '1.0rem' }}>
+            <ListGroupItem
+              id="variable2"
+              active
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[2].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[2].key)}
+            >
+              {chartVariables[2].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={2}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[2].description}
+            </MyTooltip>
+            <ListGroupItem
+              id="variable3"
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[3].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[3].key)}
+            >
+              {chartVariables[3].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={3}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[3].description}
+            </MyTooltip>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <ListGroup style={{ lineHeight: '1.0rem' }}>
+            <ListGroupItem
+              id="variable4"
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[4].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[4].key)}
+            >
+              {chartVariables[4].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={4}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[4].description}
+            </MyTooltip>
+            <ListGroupItem
+              id="variable5"
+              tag="button"
+              action
+              onClick={() =>
+                updateActive(
+                  chartVariables[5].key,
+                  active,
+                  setActive,
+                  chartData,
+                  annualFilter,
+                  selectedVol,
+                  updateSelectedChartData
+                )
+              }
+              active={active.includes(chartVariables[5].key)}
+            >
+              {chartVariables[5].title}
+            </ListGroupItem>
+            <MyTooltip
+              id={5}
+              tooltipOpen={tooltipOpen}
+              setTooltipOpen={setTooltipOpen}
+            >
+              {chartVariables[5].description}
+            </MyTooltip>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default VariableButtons;
