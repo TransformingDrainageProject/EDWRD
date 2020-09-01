026476932403fff06c84b6c457f8e57b9e2356fd
Ben Hancock <hancocb@purdue.edu>
2020-09-01 11:00:11 -0400

    add table for annual values to annual performance chart

--- client/src/components/Charts/AnnualPerformance/ChartsAnnualPerformance.js
+++ client/src/components/Charts/AnnualPerformance/ChartsAnnualPerformance.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Col, Input, Label, Row } from 'reactstrap';
+import { Button, Col, Input, Label, Row } from 'reactstrap';
 
 import AnnualChart from '../AnnualChart';
 import ChartDescription from '../ChartDescription';
 import DownloadDataButton from '../DownloadDataButton';
+import YearlyTable from '../YearlyTable';
 
 import VariableButtons from './VariableButtons';
 
@@ -11,6 +12,7 @@ const ChartsAnnualPerformance = ({ chartData }) => {
   const [activeChart, setActiveChart] = useState('');
   const [annualFilter, setAnnualFilter] = useState('all');
   const [avgLineOnly, toggleAvgLineOnly] = useState(false);
+  const [showAnnualTable, toggleShowAnnualTable] = useState(false);
 
   let selectedChartData, uniqueYears, yearRange;
 
@@ -99,21 +101,36 @@ const ChartsAnnualPerformance = ({ chartData }) => {
           </Row>
         </>
       ) : null}
-      <Row>
-        <Col>
-          <ChartDescription
-            name="annualAppliedIrrigation"
-            title="Annual Applied Irrigation"
-            text="This graph shows the annual irrigation and water quality
+      <>
+        {activeChart ? (
+          <Button
+            className="mb-3"
+            style={{ backgroundColor: '#007cb3' }}
+            onClick={() => toggleShowAnnualTable(!showAnnualTable)}
+          >{`${showAnnualTable ? 'Hide' : 'View'} Annual Values`}</Button>
+        ) : null}
+        {activeChart && showAnnualTable ? (
+          <YearlyTable data={selectedChartData.values.yearly} />
+        ) : null}
+        <Row>
+          <Col>
+            <ChartDescription
+              name="annualAppliedIrrigation"
+              title="Annual Applied Irrigation"
+              text="This graph shows the annual irrigation and water quality
                   performance metrics for a range of reservoir sizes across
                   all years at this location. Use the buttons at the top right
                   of the graph to view a specific year. Click on the question
                   mark to learn more about how these metrics are estimated."
-          >
-            <DownloadDataButton sessionID={chartData.sessionID} type="annual" />
-          </ChartDescription>
-        </Col>
-      </Row>
+            >
+              <DownloadDataButton
+                sessionID={chartData.sessionID}
+                type="annual"
+              />
+            </ChartDescription>
+          </Col>
+        </Row>
+      </>
     </>
   );
 };
