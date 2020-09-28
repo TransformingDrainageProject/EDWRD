335504b2d49fc3dd8a77f96969537e0aae24a400
Ben Hancock <hancocb@purdue.edu>
2020-09-28 10:04:51 -0400

    rename max depth to avg depth in monthly charts, change labels for reservoir water balance y-axis

--- client/src/components/Charts/ReservoirWaterBalance/ChartsReservoirWaterBalance.js
+++ client/src/components/Charts/ReservoirWaterBalance/ChartsReservoirWaterBalance.js
@@ -76,7 +76,7 @@ const ChartsFieldWaterBalance = ({ chartData }) => {
                 }`}
                 {annualFilter !== 'all' ? ` (${annualFilter})` : `${yearRange}`}
               </h1>
-              <h2>{`(Max depth = ${chartData.rdep.toFixed(1)}${
+              <h2>{`(Average depth = ${chartData.rdep.toFixed(1)}${
                 chartData.unitType === 'us' ? 'ft' : 'm'
               })`}</h2>
             </Col>
